d6d12bbc3e77edf2855fb01c48cad26bc19f8e53
Kelly <kellyc@evollve.co>
2020-09-27 09:01:35 -0500

    first commit

---
+++ visitor.js
@@ -0,0 +1,36 @@
+const express = require ("express");
+const mongoose = require ("mongoose");
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/test', { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on("error", function (err) {
+    if (err) {
+        return console.error(err);
+    }
+});
+
+const VisitorSchema= mongoose.Schema({
+    date: Date,
+    name: String,
+});
+
+const VisitorModel= mongoose.model("visitor", VisitorSchema)
+
+app.get("/", (req, res) =>{
+    const { name } = req.query;
+  
+    VisitorModel.create({
+        date: new Date(),
+        name: name ? name : "anonimo",
+    })
+    res.send("<h1>El visitante fue almacenado con exito</h1>");
+});
+
+app.listen(3000, () => {
+    console.log("server listening on port 3000")
+});
